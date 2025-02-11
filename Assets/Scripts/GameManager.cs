6dde6223fcc7bed04f69a85173b15d043f1dcda5
fetrentin7 <fetrentin7@gmail.com>
2025-02-11 12:27:15 -0300

    mouse hovering and destroying

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private float spawnrate = 1.0f;
+    public List<GameObject> targets;
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnrate);
+            int index = Random.Range(0, targets.Count); ;
+            Instantiate(targets[index]);
+        }
+    }
+}
--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -4,19 +4,51 @@ public class Target : MonoBehaviour
 {
     //tossing them 4 targets into the air with a random force, torque and position
     private Rigidbody targetRb;
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float maxTorque = 10;
+    private float xRange = 4.0f;
+    private float ySpawnPos = -6.0f;
+
 
     void Start()
     {
         targetRb = GetComponent<Rigidbody>();
-        targetRb.AddForce(Vector3.up * Random.Range(12, 16), ForceMode.Impulse);
-        targetRb.AddTorque(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10), ForceMode.Impulse);
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        targetRb.AddTorque(RandomTorque(), Random.Range(-maxTorque, maxTorque), 
+            Random.Range(-maxTorque, maxTorque), ForceMode.Impulse);
 
-        transform.position = new Vector3(Random.Range(-4, 4), -6, 0);
+        transform.position = new Vector3(Random.Range(-xRange, xRange), ySpawnPos); //random place for the object to spawn
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    float RandomTorque()
+    {
+       return Random.Range(-maxTorque, maxTorque);
+    }
+
+    Vector3 RandomPosition()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
     }
 }
