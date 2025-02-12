53004d3fa1b1615bae69c61b4e1480b4c4ac6821
fetrentin7 <fetrentin7@gmail.com>
2025-02-12 09:40:13 -0300

    button

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,18 +3,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    
+
     public List<GameObject> targets;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public Button restartButton;
 
     private float spawnrate = 1.0f;
     private int score;
+    public bool isGameActive;
 
     void Start()
     {
+        isGameActive = true;
         StartCoroutine(SpawnTarget());
         score = 0;
         UpdateScore(0);
@@ -23,17 +29,16 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     IEnumerator SpawnTarget()
     {
-        while (true)
+        while (isGameActive)
         {
             yield return new WaitForSeconds(spawnrate);
-            int index = Random.Range(0, targets.Count); 
+            int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
-            
         }
     }
 
@@ -41,5 +46,17 @@ public class GameManager : MonoBehaviour
     {
         score += scoreToAdd;
         scoreText.text = "Score " + score;
+    }
+
+    public void GameOver()
+    {
+        restartButton.gameObject.SetActive(true);
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -3,16 +3,19 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     //tossing them 4 targets into the air with a random force, torque and position
-    private Rigidbody targetRb;
-    private float minSpeed = 12;
-    private float maxSpeed = 16;
-    private float maxTorque = 5;
+    
+    private float minSpeed = 12.0f;
+    private float maxSpeed = 16.0f;
+    private float maxTorque = 2.0f;
     private float xRange = 4.0f;
     private float ySpawnPos = -6.0f;
+
     private GameManager gameManager;
+    private Rigidbody targetRb;
+
+    public ParticleSystem expolosionParticle;
 
     public int pointValue;
-
     void Start()
     {
         gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
@@ -23,7 +26,6 @@ public class Target : MonoBehaviour
             Random.Range(-maxTorque, maxTorque), ForceMode.Impulse);
 
         transform.position = new Vector3(Random.Range(-xRange, xRange), ySpawnPos); //random place for the object to spawn
-        
     }
 
     // Update is called once per frame
@@ -49,12 +51,21 @@ public class Target : MonoBehaviour
 
     private void OnMouseDown()
     {
-        Destroy(gameObject);
-        gameManager.UpdateScore(5);
+        if (gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            Instantiate(expolosionParticle, transform.position, expolosionParticle.transform.rotation);
+            gameManager.UpdateScore(pointValue);
+        }
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
+        if (!gameObject.CompareTag("Bad")) //if it's not bnad
+        {
+            gameManager.GameOver();
+        }
     }
 }
